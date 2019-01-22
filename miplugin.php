c6fdb98d8ac3eab9ef7888f00818e85f30b47ce2
Luisfrb56 <luisfrb56@gmail.com>
2019-01-22 16:57:53 +0000

    plugin con shortcode, uso de base de datos

---
+++ miplugin.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @package Mi_Plugin
+ * @version 2.0
+ */
+/*
+Plugin Name: MiPlugin2.0
+Plugin URI: http://wordpress.org/plugins/mi-plugin/
+Description: Hace cosas
+Author: Luis Fernando
+Version: 2.0
+Author URI: http://luis.tt/
+*/
+
+
+
+function errores_shortcode_init(){
+function mal_sonantes ($atts = [],$content=null ) {
+
+global $wpdb;
+
+        $charset_collate = $wpdb->get_charset_collate();
+        $table_name = $wpdb->prefix . 'pluginerrores';
+
+        $sql = "CREATE TABLE IF NOT EXISTS $table_name(
+        fallo varchar(80),
+        correccion varchar(80),
+        PRIMARY KEY(fallo)
+        ) $charset_collate;";
+
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+
+
+}
+add_shortcode('error', 'mal_sonantes');
+}
+
+add_filter( 'init', 'errores_shortcode_init' );
+
+
+
+function nota_final( $content ) {
+	$content .= '<footer">Gracias por leer mi post, confio en que elijas otro que te guste para visitar.</footer>';
+	return $content;
+}
+add_filter( 'the_content', 'nota_final' );
+
+function corregir( $text ) {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'pluginerrores';
+    $results1 = $wpdb->get_results( "SELECT fallo FROM $table_name", OBJECT );
+    $results2 = $wpdb->get_results( "SELECT correccion FROM $table_name", OBJECT );
+	
+    $search = array();
+    $replace= array();
+    
+	for($i=0;$i<count($results1); $i++) {
+        array_push($search,$results1[$i]->fallo);
+        array_push($replace,$results2[$i]->correccion);
+	};
+	return str_replace( $search, $replace, $text );
+}
+add_filter( 'the_content', 'corregir' );
